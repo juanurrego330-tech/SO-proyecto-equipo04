3d8a03664219304e13292c3039cac9dcebf44c53
juanj <juanj@LAPTOP-BS0QDBE1>
2026-03-27 18:38:44 -0500

    Visual

---
+++ Gestor de Procesos y Concurrencia(Visual)/Gestor de Procesos y Concurrencia(Visual)/Form1.cs
@@ -0,0 +1,10 @@
+namespace Gestor_de_Procesos_y_Concurrencia_Visual_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
