b3dcd0d2c30344ae6b70185c62a78aa6ed19dabc
juanj <juanj@LAPTOP-BS0QDBE1>
2026-04-01 23:01:26 -0500

    Inicio Interfaz Grafica

--- src/Gestor de Procesos y Concurrencia(Visual)/Gestor de Procesos y Concurrencia(Visual)/Form1.Designer.cs
+++ src/Gestor de Procesos y Concurrencia(Visual)/Gestor de Procesos y Concurrencia(Visual)/Form1.Designer.cs
@@ -28,12 +28,275 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            button2 = new Button();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            tabPage3 = new TabPage();
+            tabPage4 = new TabPage();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.Font = new Font("Segoe UI", 14F);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(229, 260);
+            label1.Name = "label1";
+            label1.Size = new Size(349, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Ingrese el numero de procesos:";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveCaptionText;
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(248, 311);
+            label2.Name = "label2";
+            label2.Size = new Size(330, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Ingrese el valor del Quantum:";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.None;
+            textBox1.Location = new Point(584, 265);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.None;
+            textBox2.Location = new Point(584, 316);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.None;
+            button1.Location = new Point(584, 372);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 28);
+            button1.TabIndex = 4;
+            button1.Text = "Aceptar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.ItemSize = new Size(140, 30);
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1071, 634);
+            tabControl1.SizeMode = TabSizeMode.Fixed;
+            tabControl1.TabIndex = 5;
+            tabControl1.Selecting += tabControl1_Selecting;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = Color.Black;
+            tabPage1.BackgroundImageLayout = ImageLayout.None;
+            tabPage1.BorderStyle = BorderStyle.Fixed3D;
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(textBox2);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Font = new Font("Segoe UI", 9F);
+            tabPage1.Location = new Point(4, 34);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1063, 596);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Inicio";
+            // 
+            // tabPage2
+            // 
+            tabPage2.BackColor = Color.Black;
+            tabPage2.Controls.Add(textBox5);
+            tabPage2.Controls.Add(textBox4);
+            tabPage2.Controls.Add(textBox3);
+            tabPage2.Controls.Add(button2);
+            tabPage2.Controls.Add(label6);
+            tabPage2.Controls.Add(label5);
+            tabPage2.Controls.Add(label4);
+            tabPage2.Controls.Add(label3);
+            tabPage2.Location = new Point(4, 34);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1063, 596);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Ingreso Datos";
+            // 
+            // textBox5
+            // 
+            textBox5.Anchor = AnchorStyles.None;
+            textBox5.Location = new Point(472, 316);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 27);
+            textBox5.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.None;
+            textBox4.Location = new Point(472, 270);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 27);
+            textBox4.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.None;
+            textBox3.Location = new Point(472, 223);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 27);
+            textBox3.TabIndex = 6;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.None;
+            button2.Location = new Point(472, 367);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 27);
+            button2.TabIndex = 9;
+            button2.Text = "Aceptar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.None;
+            label6.AutoSize = true;
+            label6.BackColor = Color.Black;
+            label6.Font = new Font("Segoe UI", 14F);
+            label6.ForeColor = SystemColors.ButtonHighlight;
+            label6.Location = new Point(337, 311);
+            label6.Name = "label6";
+            label6.Size = new Size(133, 32);
+            label6.TabIndex = 3;
+            label6.Text = "Burst Time:";
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.None;
+            label5.AutoSize = true;
+            label5.BackColor = Color.Black;
+            label5.Font = new Font("Segoe UI", 14F);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(324, 265);
+            label5.Name = "label5";
+            label5.Size = new Size(146, 32);
+            label5.TabIndex = 2;
+            label5.Text = "Arrival Time:";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.None;
+            label4.AutoSize = true;
+            label4.BackColor = Color.Black;
+            label4.Font = new Font("Segoe UI", 14F);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(428, 217);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 32);
+            label4.TabIndex = 1;
+            label4.Text = "ID:";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.None;
+            label3.AutoSize = true;
+            label3.BackColor = Color.Black;
+            label3.Font = new Font("Segoe UI", 14F);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(342, 175);
+            label3.Name = "label3";
+            label3.Size = new Size(131, 32);
+            label3.TabIndex = 0;
+            label3.Text = "Proceso #1";
+            // 
+            // tabPage3
+            // 
+            tabPage3.BackColor = Color.Black;
+            tabPage3.Location = new Point(4, 34);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(1063, 596);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Datos Ingresados";
+            // 
+            // tabPage4
+            // 
+            tabPage4.BackColor = Color.Black;
+            tabPage4.Location = new Point(4, 34);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Size = new Size(1063, 596);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Ejecucion";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(1071, 634);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Gestor de Procesos y Concurrencia";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private Label label3;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Button button2;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
     }
 }
--- src/Gestor de Procesos y Concurrencia(Visual)/Gestor de Procesos y Concurrencia(Visual)/Form1.cs
+++ src/Gestor de Procesos y Concurrencia(Visual)/Gestor de Procesos y Concurrencia(Visual)/Form1.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace Gestor_de_Procesos_y_Concurrencia_Visual_
 {
     public partial class Form1 : Form
@@ -5,6 +8,112 @@ namespace Gestor_de_Procesos_y_Concurrencia_Visual_
         public Form1()
         {
             InitializeComponent();
+        }
+        int quantum;
+        int numeroProcesos;
+        int contadorProcesos = 1;
+        bool cambioPermitido = false;
+        private List<string[]> listaProcesos = new List<string[]>();
+        public bool ValidarCampos(string a)
+        {
+            if (a == "")
+            {
+                MessageBox.Show("Por favor, complete todos los campos.");
+                return false;
+            }
+            return true;
+        }
+        public bool ValidarNumerosInicio()
+        {
+            if (!int.TryParse(textBox1.Text, out int numeroproces))
+            {
+                MessageBox.Show("Por favor, ingrese un número válido de procesos (Entero)");
+                return false;
+            }
+            if (!int.TryParse(textBox2.Text, out int valorquantum))
+            {
+                MessageBox.Show("Por favor, ingrese un número válido para el quantum (Entero)");
+                return false;
+            }
+            numeroProcesos = numeroproces;
+            quantum = valorquantum;
+            return true;
+        }
+        public bool ValidarNumerosIngresoDatos()
+        {
+            if (!int.TryParse(textBox4.Text, out int arrivaltime))
+            {
+                MessageBox.Show("Por favor, ingrese un número válido de Arrival (Entero)");
+                return false;
+            }
+            if (!int.TryParse(textBox5.Text, out int bursttime))
+            {
+                MessageBox.Show("Por favor, ingrese un número válido para el Burst (Entero)");
+                return false;
+            }
+            return true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ValidarCampos(textBox1.Text) && ValidarCampos(textBox2.Text))
+            {
+                if (ValidarNumerosInicio())
+                {
+                    cambioPermitido = true;
+                    tabControl1.SelectedTab = tabPage2;
+                    cambioPermitido = false;
+                }
+            }
+        }
+        public void limpiarCampos()
+        {
+            label3.Text = "Proceso #" + contadorProcesos;
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+
+        public void GuardarDatosProceso()
+        {
+            string[] Proceso = new string[4];
+            Proceso[0] = textBox3.Text;
+            Proceso[1] = "Arrival Time: " + textBox4.Text;
+            Proceso[2] = "Burst Time: " + textBox5.Text;
+            listBox1.Items.Add(Proceso);
+        }
+        /*
+        int id = contadorProcesos;
+        int tiempoLlegada = int.Parse(textBox3.Text);
+        int rafaga = int.Parse(textBox4.Text);
+        int prioridad = int.Parse(textBox5.Text);
+        Proceso nuevoProceso = new Proceso(id, tiempoLlegada, rafaga, prioridad);
+        listBox1.Items.Add(nuevoProceso);
+        contadorProcesos++;
+        */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ValidarCampos(textBox3.Text) && ValidarCampos(textBox4.Text) && ValidarCampos(textBox5.Text))
+            {
+                if (ValidarNumerosIngresoDatos())
+                {
+                    contadorProcesos++;
+                    limpiarCampos();
+                    if (contadorProcesos > numeroProcesos)
+                    {                        
+                        cambioPermitido = true;
+                        tabControl1.SelectedTab = tabPage3;
+                        cambioPermitido = false;
+                    }
+                }
+            }
+        }
+
+        private void tabControl1_Selecting(object sender, TabControlCancelEventArgs e)
+        {
+            if (!cambioPermitido)
+            {
+                e.Cancel = true;
+            }
         }
     }
 }
